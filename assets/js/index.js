0e806d0687dca132dbb5db1223b452a2a795563a
namanTiwari <er.amantiwari.2002@gmail.com>
2023-04-27 02:01:37 +0530

    Added Video Element Using Js Navigator

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,24 @@
 console.log("Scipt is running");
 
+const videoGrid =document.getElementById('video-grid')
 
+const myVideo = document.createElement('video');
+myVideo.muted = false
+
+let myVedioStream
+navigator.mediaDevices.getUserMedia({
+    video : true,
+    audio : true
+})
+.then(stream=>{
+    myVedioStream = stream;
+    addVideoStream(myVideo,stream)
+});
+
+const addVideoStream = (video,stream)=>{
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata',()=>{
+        video.play(); 
+    })
+    videoGrid.append(video);
+}
